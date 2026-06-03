7d9cbc287891d48e1bd5961e963a955ba8642f23
thvarzil <louisblebohec@outlook.com>
2026-06-03 12:39:16 -0700

    add coastal proximity influence with squared falloff

--- generator/moisture.py
+++ generator/moisture.py
@@ -3,9 +3,19 @@ import opensimplex
 
 from hex_grid import HexGrid
 
-def generate_moisture(hexgrid:HexGrid, seed, octaves=6, persistence=0.5, lacunarity=2.0, offset=0.0):
+def generate_moisture(
+        hexgrid:HexGrid, 
+        seed, 
+        base_humidity           =0.0,
+        coast_influence_radius  =20,
+        lacunarity              =2.0, 
+        octaves                 =6, 
+        persistence             =0.5, 
+        ):
     opensimplex.seed(seed)
     hexes = hexgrid.grid
+    
+
 
     for y in range(hexgrid.ysize):
         for x in range(hexgrid.xsize):
@@ -13,6 +23,8 @@ def generate_moisture(hexgrid:HexGrid, seed, octaves=6, persistence=0.5, lacunar
             amplitude = 1.0
             frequency = 1.0
             max_value = 0.0
+
+            coastal_bonus = max(0, 1 - hexes[(x,y)].distance_to_ocean / coast_influence_radius) ** 2
 
             for _ in range(octaves):
                 moisture += opensimplex.noise2(
@@ -23,4 +35,4 @@ def generate_moisture(hexgrid:HexGrid, seed, octaves=6, persistence=0.5, lacunar
                 amplitude *= persistence
                 frequency *= lacunarity
 
-            hexes[(x,y)].moisture = moisture/max_value + offset
+            hexes[(x,y)].moisture = moisture/max_value + base_humidity + coastal_bonus
