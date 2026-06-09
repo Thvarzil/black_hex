5ff2b8820a03217f7ccae72b080f9ff9b5420a58
thvarzil <louisblebohec@outlook.com>
2026-06-09 14:03:39 -0700

    dirty wip commit

--- generator/temperature.py
+++ generator/temperature.py
@@ -4,29 +4,28 @@ from hex_grid import HexGrid
 
 BASE_SCALE=0.04
 
-def generate_elevation(hexgrid:HexGrid, seed, octaves=6, persistence=0.5, lacunarity=2.0, offset=0.1):
+def generate_temperature(hexgrid:HexGrid, seed, octaves=6, persistence=0.5, lacunarity=2.0):
 
     opensimplex.seed(seed)
     hexes = hexgrid.grid
 
     for y in range(hexgrid.ysize):
         for x in range(hexgrid.xsize):
-            elevation = 0.0
+            temperature = 0.0
             amplitude = 1.0
             frequency = 1.0
             max_value = 0.0
 
             for _ in range(octaves):
-                elevation += opensimplex.noise2(
+                temperature += opensimplex.noise2(
                     x=(x+0.5)* BASE_SCALE * frequency,
                     y=(y+0.5)* BASE_SCALE * frequency,
                     ) * amplitude
                 max_value += amplitude
                 amplitude *= persistence
                 frequency *= lacunarity
+            
+            offset=0.1 #TODO calc elevation and latitude offsets
+            hexes[(x,y)].temperature = temperature/max_value + offset
 
-            hexes[(x,y)].elevation = elevation/max_value + offset
-            hexes[(x,y)].elevation_m = int(hexes[(x,y)].elevation * 9_000) # Approximation of everest (8_800m) and mariana trench (-11_000m)
-            if hexes[(x,y)].elevation < 0:
-                hexes[(x,y)].biome = "Ocean"
-                hexes[(x,y)].distance_to_ocean = 0
+    return "foo"
--- hex.py
+++ hex.py
@@ -5,5 +5,7 @@ class Hex:
     biome: str = ""
     distance_to_ocean:int = None
     elevation: float = 0.0
+    elevation_m: int = 0 
     moisture: float = 0.0
     temperature: float = 0.0
+    temperature_c: float = 0.0
